bbb655d08086d51c0c47f8b7ee13177aa807ba17
Philip Lindberg <philip.lindberg101@gmail.com>
2017-04-22 07:32:40 +0300

    Inits.

--- LudumDare38/core/src/com/gurbx/ld38/Application.java
+++ LudumDare38/core/src/com/gurbx/ld38/Application.java
@@ -1,27 +1,57 @@
 package com.gurbx.ld38;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.gurbx.ld38.util.Constants;
 
-public class Application extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Application extends Game {
+	public SpriteBatch batch;
+	public OrthographicCamera camera, uiCamera;
+	public Viewport viewport, uiViewport;
+	public AssetManager assets;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		assets = new AssetManager();
+		camera = new OrthographicCamera();
+		uiCamera = new OrthographicCamera();
+		viewport = new StretchViewport(Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
+		uiViewport = new StretchViewport(Constants.UI_VIRTUAL_WIDTH, Constants.UI_VIRTUAL_HEIGHT, uiCamera);
+		viewport.apply();
+		uiViewport.apply();
+		camera.position.set(Constants.VIRTUAL_WIDTH/2, Constants.VIRTUAL_HEIGHT/2, 0);
+		uiCamera.position.set(Constants.UI_VIRTUAL_WIDTH/2, Constants.UI_VIRTUAL_HEIGHT/2, 0);
+		camera.update();
+		uiCamera.update();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+		uiViewport.update(width, height);
+		super.resize(width, height);
 	}
 
 	@Override
 	public void render () {
+		super.render();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		batch.dispose();
+		assets.dispose();
 	}
 }
---
+++ LudumDare38/core/src/com/gurbx/ld38/util/Constants.java
@@ -0,0 +1,9 @@
+package com.gurbx.ld38.util;
+
+public class Constants {
+	public final static int VIRTUAL_WIDTH = 800;
+	public final static int VIRTUAL_HEIGHT = 800;
+	public final static int UI_VIRTUAL_WIDTH = 800;
+	public final static int UI_VIRTUAL_HEIGHT = 800;
+
+}
